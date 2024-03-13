c88a8c2b989887e47077045c1e614696c2c27930
KiranRiaz57 <159231693+KiranRiaz57@users.noreply.github.com>
2024-03-13 22:20:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+// Q22
+// let friends: string[] = ["Kiran","Mahrang","Laiba"]
+// international error;
+// console.log(friends[3])
+// correcting the error;
+// console.log(friends[2]="Laiba")
+// Q23
+// conditional test
+// let car = "subaru"
+// console.log("Is car =='subaru'? I predict True.")
+// console.log(car=='subaru')
+// console.log("Is car =='toyota'? I predict False.")
+// console.log(car=='toyota')
+// let color = "red"
+// console.log("Is color == 'red' I predict True")
+// console.log(color=='red')
+// console.log("Is color == 'yellow' I predict False ")
+// console.log(color=='yellow')
+// let mobile = "vivo"
+// console.log("Is mobile == 'vivo' I predict True")
+// console.log(mobile=='vivo')
+// console.log("Is mobile == 'samsung' I predict False")
+// console.log(mobile=='samsung')
+// let laptop = "dell"
+// console.log("Is laptop=='dell' I predict True")
+// console.log(laptop=='dell')
+// console.log("Is laptop=='hp' I predict False")
+// console.log(laptop=='hp')
+// let fridge = "haier"
+// console.log("Is fridge=='haier' I predict True")
+// console.log(fridge=='haier')
+// console.log("Is fridge=='dawlance' I predict False")
+// console.log(fridge=='dawlance')
+// Q24
+// Equality with strings
+// console.log("Testing equality with strings:")
+// console.log("apple"=="apple")
+// console.log("apple"=="Apple")
+// using the lower case function
+// console.log("Testing with lower case:")
+// console.log("Apple".toLowerCase()=="apple")
+// console.log("Apple".toLowerCase()=="APPLE")
+// Numerical tests
+// console.log(" Numerical tests")
+// console.log(10>5)
+// console.log(2<1)
+// Testing using "and" and "or"operators
+// console.log("Testing with 'and' and 'or'")
+// console.log(true && false)
+// Testing items is in array
+let fruitsList = ["apple", "mango", "banna"];
+console.log("Is 'apple'in fruits?");
+console.log(fruitsList.includes("apple"));
+console.log("Is 'cherry' not in fruits?");
+console.log(!fruitsList.includes("cherry"));
